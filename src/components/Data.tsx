baacd0c2604e20f93a9af0592e98ceddd771fb07
Harsh V Srivastava <harshvsri@gmail.com>
2023-07-06 13:28:21 +0530

    Modify Card - add images

--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -34,43 +34,41 @@ export const projects = [
   {
     name: "ShoeKart",
     about:
-      "Shoekart is a repository showcasing code and resources for a shoe e-commerce platform. Built with React, TypeScript, and Vite, it offers a seamless online shopping experience. Users can browse, select, and purchase footwear effortlessly. With the power of modern technologies, Shoekart ensures a smooth and efficient e-commerce solution for shoe enthusiasts.",
+      "Modern shoe e-commerce platform built with React, TypeScript, and Vite.",
     liveLink: "https://shoe-kart-harshvsri.netlify.app",
     sourceCode: "https://github.com/harshvsri/shoekart",
   },
   {
-    name: "Task List",
+    name: "TaskList",
     about:
-      "Task List is a user-friendly to-do list app built with React, TypeScript, and Vite. It offers efficient task management, allowing easy task creation, editing, and organization. Features like prioritization, due dates, reminders, and categorization provide flexibility. With a responsive UI and seamless design, Task List ensures a reliable and enjoyable task management experience.",
+      "User-friendly to-do list app built with React, TypeScript, and Vite.",
     liveLink: "https://task-list-harshvsri.netlify.app/",
     sourceCode: "https://github.com/harshvsri/task-list",
   },
   {
-    name: "Memory Card",
-    about:
-      "Challenge yourself in the Card Selection Game, putting your memory and strategy to the test. Exercise smart decision-making and sharp memory skills as you choose 12 unique cards without any repetition. Can you conquer the game and showcase your unmatched card selection prowess? Take on the challenge now!",
+    name: "MemoryCard",
+    about: "Challenge your memory and strategy in a card selection game.",
     liveLink: "https://memory-card-harshvsri.netlify.app",
     sourceCode: "https://github.com/harshvsri/memory-card",
   },
   {
     name: "HarshFlix",
     about:
-      "It utilizes the OMDB (Open Movie Database) API to provide a user-friendly and intuitive interface for searching and displaying movie details. With its sleek design and smooth functionality, users can effortlessly explore a vast collection of movies, accessing comprehensive information. The app leverages React to deliver a seamless user experience.",
+      "User-friendly movie search app utilizing the OMDB API, providing movie details.",
     liveLink: "https://harshflix.netlify.app",
     sourceCode: "https://github.com/harshvsri/harshflix",
   },
   {
     name: "Keeper",
     about:
-      "Built with React, this Google Keep clone app offers a user-friendly interface for organizing and managing notes. With features like creating, editing, and categorizing notes, it provides a seamless experience similar to the popular Google Keep app. Stay organized and boost productivity with this convenient note-taking tool.",
+      "Google Keep clone for organizing and managing notes, boosting productivity.",
     liveLink: "https://notekeeperapp-harsh.netlify.app",
     sourceCode: "https://github.com/harshvsri/keeper-app",
   },
   {
-    name: "Tic Tac Toe",
-    about:
-      "Immerse yourself in the fun and strategic gameplay of Tic Tac Toe with this web-based version. Crafted using vanilla JavaScript and DOM manipulation, it brings the classic game to life in your browser. Compete against friends or challenge the AI, and experience the nostalgia and excitement of this beloved game right at your fingertips",
-    liveLink: "https://harshvsri.github.io/tic-tac-toe/",
-    sourceCode: "https://github.com/harshvsri/tic-tac-toe",
+    name: "Tindog",
+    about: "Fun landing page introducing a Tinder-like platform for dogs.",
+    liveLink: "https://harshvsri.github.io/tindog/",
+    sourceCode: "https://github.com/harshvsri/tindog",
   },
 ];
